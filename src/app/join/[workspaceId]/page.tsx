6bc732a7da7ed3488141cd4a35230a0da47c3e5b
stefantrenda <stefantrenda@gmail.com>
2025-02-20 22:10:32 +0100

    contitnue

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,21 +1,60 @@
 "use client";
 
+import Link from "next/link";
 import Image from "next/image";
-
+import { Loader } from "lucide-react";
+import { useRouter } from "next/router";
 import VerificationInput from "react-verification-input";
 
+import { useJoin } from "@/features/workspaces/api/use-join";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { toast } from "sonner";
+
 const JoinPage = () => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+
+  const { mutate, isPendig } = useJoin();
+  const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
+
+  const handleComplete = (value) => {
+    mutate(
+      { workspaceId, joinCode: value },
+      {
+        onSuccess: (id) => {
+          router.replace("")
+          toast.success("Workspace joined");
+        },
+        onError: () => {
+          toast.error("failed to join workspace");
+        },
+      }
+    );
+  };
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
   return (
     <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md">
       <Image src={"/logo.svg"} width={60} height={60} alt="logo" />
       <div className="flex flex-col gap-y-4 items-center justify-center max-x-md">
         <div className="flex flex-col gap-y-2 items-center justify-center">
-          <h1 className="text-2xl font-bold">Join workspace</h1>
+          <h1 className="text-2xl font-bold">Join {data?.name}</h1>
           <p className="text-md text-muted-foreground">
             Enter the workspace code to join
           </p>
         </div>
         <VerificationInput
+          length={6}
           classNames={{
             container: "flex gap-x-2",
             character:
@@ -24,7 +63,13 @@ const JoinPage = () => {
             characterFilled: "bg-white text-black",
             characterSelected: "bg-white text-black",
           }}
+          autoFocus
         />
+      </div>
+      <div className="flex gap-x-4 ">
+        <Button size={"lg"} variant={"outline"} asChild>
+          <Link href="/">Back to home</Link>
+        </Button>
       </div>
     </div>
   );
---
+++ src/features/workspaces/api/use-get-workspace-info.ts
@@ -0,0 +1,17 @@
+import { Id } from "../../../../convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+
+interface UseGetWorkspaceInfoProps {
+  id: Id<"workspaces">;
+}
+
+export const useGetWorkspaceInfo = ({ id }: UseGetWorkspaceInfoProps) => {
+  const data = useQuery(api.workspaces.getInfoById, { id });
+  const isLoading = data === undefined;
+
+  return {
+    data,
+    isLoading,
+  };
+};
