f8b1e57d9adca9e166df11fe770db05cfcc017f8
stefantrenda <stefantrenda@gmail.com>
2025-04-07 16:21:17 +0200

    continue

--- src/components/thumbnail.tsx
+++ src/components/thumbnail.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
-
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/dialog";
+} from "@/components/ui/dialog";
 import { XIcon } from "lucide-react";
 
 interface ThumbnailProps {
@@ -17,12 +16,23 @@ export const Thumbnail = ({ url }: ThumbnailProps) => {
   if (!url) return null;
 
   return (
-    <div className="relative overflow-hidden max-w-[360px] border rounded-lg my-2 cursor-zoom-in">
-      <img
-        src={url}
-        alt="Message iamge"
-        className="rounded-md object-cover size-full"
-      />
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="relative overflow-hidden max-w-[360px] border rounded-lg my-2 cursor-zoom-in">
+          <img
+            src={url}
+            alt="Message iamge"
+            className="rounded-md object-cover size-full"
+          />
+        </div>
+      </DialogTrigger>
+      <DialogContent className="max-w-[800px] border-none bg-transparent p-0 shadow-none">
+        <img
+          src={url}
+          alt="Message iamge"
+          className="rounded-md object-cover size-full"
+        />
+      </DialogContent>
+    </Dialog>
   );
 };
